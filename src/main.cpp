34052d27e6d2f88b6e3197c692fc16f2308e7424
Louna_Bashti <bashlodu59@orange.fr>
2024-01-31 17:11:12 +0100

    commit final

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+int SensorValue;
+int SensorLow = 1023;
+int SensorHigh = 0;
 
-void setup() {
+const int ledPin = 13;
+
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+
+  while (millis() < 5000)
+  {
+    SensorValue = analogRead(A0);
+    if (SensorValue > SensorHigh)
+    {
+      SensorHigh = SensorValue;
+    }
+    if (SensorValue < SensorLow)
+    {
+      SensorLow = SensorValue;
+    }
+  }
+  digitalWrite(ledPin, LOW);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  SensorValue = analogRead(A0);
+  int pitch =  map(SensorValue, SensorLow, SensorHigh, 50, 4000);
+  tone(8, pitch, 20);
+  delay(10);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
